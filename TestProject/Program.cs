e049230505c765ceba0213f20a1cefeff1087705
Riki-lu <r0527618125@gmail.com>
2022-08-09 02:46:39 +0300

    Step1_FindCD

---
+++ TestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static TestProject.Algorithm;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string demoQuery = "Connections| where DestinationEntityType =~'AadIdentity'| extend a = a + b |let x= where a > 10.0";
+        var cleanQuery = PassQueryFindCustomerData(demoQuery);
+        Console.WriteLine("Source query: "+demoQuery);
+        Console.WriteLine("New query: " + cleanQuery);
+    }
+}
